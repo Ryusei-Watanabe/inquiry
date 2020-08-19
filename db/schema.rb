475bbf098aab2772d401c93c5a76fecdbe93e0bb
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-08-19 17:07:08 +0900

    work

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(version: 2020_08_19_071125) do
+  enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
